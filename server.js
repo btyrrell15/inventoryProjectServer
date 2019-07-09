877f57d3b671c81f210046e69dacbc26a32dc80d
Brian Tyrrell <btyrrell15@hotmail.com>
2019-07-09 09:30:49 -0600

    Update item is working

--- server.js
+++ server.js
@@ -44,6 +44,50 @@ server.post("/inventory", function(req, res){
     });
 });
 
+server.put("/inventory/:id", function(req, res){
+    inventoryModel.findById(req.params.id).then(function(inventory){
+        if (inventory == null){
+            res.status(404);
+            res.json({
+                msg: `There is no item with the ID of ${req.params.id}`
+            });
+        } else {
+            if (req.body.sku != undefined){
+                inventory.sku = req.body.sku;
+            }
+            if (req.body.image != undefined){
+                inventory.image = req.body.image;
+            }
+            if (req.body.title != undefined){
+                inventory.title = req.body.title;
+            }
+            if (req.body.category != undefined){
+                inventory.category = req.body.category;
+            }
+            if (req.body.marketplace != undefined){
+                inventory.marketplace = req.body.marketplace;
+            }
+            if (req.body.quantity != undefined){
+                inventory.quantity = req.body.quantity;
+            }
+            if (req.body.cost != undefined){
+                inventory.cost = req.body.cost;
+            }
+            if (req.body.location != undefined){
+                inventory.location = req.body.location;
+            }
+            inventory.save().then(function(){
+                res.status(200);
+                res.json({
+                    inventory: inventory
+                });
+            });
+        }
+    }).catch(function(error){
+        res.status(400).json({msg: error.message});
+    });
+});
+
 
 
 //start the server and connect to database
