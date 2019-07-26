1b55da94772b7eb60cfe4f16625e35379ed99320
Grayson Ercanbrack <graymbp@GrayMBP.local>
2019-07-26 09:14:16 -0600

    finished last function

--- server.js
+++ server.js
@@ -408,14 +408,12 @@ var fetchEbay = function(ebayUrl) {
         headers: {"Authorization": "Bearer v^1.1#i^1#f^0#r^0#I^3#p^3#t^H4sIAAAAAAAAAOVYf2zUVBzfbTdkwFg0RggQvBTF8KN37fV6P8p2esfGOIFt7AbCMhyv7etW1mvP9nXbCdFlyYgiYEgkGhEdIhgJ0ahIIpFEEiSS+AshMRgwGhMyM1DMyJg/En3ttuM249jdiFni/dN7r99fn+/3877vvVIdU4oWb1+5/Wax46787g6qI9/hoKdTRVMKl8wsyJ9TmEdlCDi6Ox7ocHYW9JQaIKEkuVpoJDXVgK72hKIanD1ZRpi6ymnAkA1OBQlocEjg4pE1qzmvm+KSuoY0QVMIV6y8jOChHwBBlEI+r0gzTBDPqsM267QyAsJgUAwCUWB9VCjIBPB7wzBhTDUQUFEZ4aXoEEkFSK+/jmY5iuVo1h2gQvWEaz3UDVlTsYibIsJ2uJytq2fEOnaowDCgjrARIhyLrIhXR2LlFVV1pZ4MW+GhPMQRQKYxcrRcE6FrPVBMOLYbw5bm4qYgQMMgPOFBDyONcpHhYHII3061xEsMI0qS1+tjgyzruyOpXKHpCYDGjsOakUVSskU5qCIZpW6XUZwNfgsU0NCoCpuIlbusx1oTKLIkQ72MqIhGNq6LV9QSrnhNja61yiIULaQ042NoNhQKEGEEDZxCqDfKaiv2rOkp75C3QZNDuR7lbrmmirKVOcNVpaEoxKHDkQnycWxGgrBQtVqtRyRkhZUpF0gn0l9vVXawlCZqVq3iwgSOyWUPb1+GYV7cYsKdYgbjFfyCX+LFkEDxAdqbyQxrrefKjrBVoEhNjceKBfIgRSaA3gJRUgECJAWcXjMBdVnkGFbyMkEJkqI/JJG+kCSRPCv6SVqCkIKQ54VQ8H9HEoR0mTcRTBNl9AsbKW6ROLGcDCQOaS1QrUslITFa0m5CQ+xoN8qIZoSSnMfT1tbmbmPcmt7k8VIU7dmwZnVcaIYJQKRl5dsLk7JNXQFiLUPmEA6gjGjHNMTO1SYiXFuxorYivrKxrnpVRdUwhUdEFh49+y9I44KWhDWaIgupyQWR0cUaoKNU1EzhcRwqCn5MCKphQf2vQdprfUyglg0DGwFJ2W3xzi1oCY8GcAOzphrtqF3jEfLwZgrHIELdrUMgaqqSGr9ek4kX7KD2+JQMXBH3YO/BMLL0OFI5C510N8nFYVo5Cx0gCJqpolzcDalmoSGZiiQritWYcnGYoZ5NmCpQUkgWjNxraG8+OL2G3NSMsrWD5/COhfUFgICiZUsli7xGs5ZMWiwUcMfIYq1IEl4rwBTsjT67YPGWZx+6MqO11npONnCnkJVc05a2kmzWVDhhK0AUdXxknrAd64SUE4ll1WqWxoRafSSZjCUSJgK8AmPiJNvYGIpmQhOGN8lQRXUZ4IOSTg43WTIe3UCKYlAICAG/QPqAl/LzPmlCuMth62TB7ezM3zSMHSOkg2zIh4/aIET6IMOQPC/5ySAf4FmfjwpIgJ0Q7uWKjHM6+Y6hKzUDQXFi0PClaXKBsnk7RFuWEXAtWT8gfSzE/wLWBUMEwfFCHjWRce34x7XTM/LjTzjP/tGdjpNUp+PDfIeDClAkvYRaNKVgnbNgBmHICLoNoIq81u7GVxU33nxVgEwdultgKglkPX+KQ/72gjCQ8dmpexM1O/3hqaiAnp7xFYqad+tNIV0yq5gOUQGvn2YplmbrqQW33jrp+5z3fvVzlbs4euqz/iNT37lxPXi8r6twLlWcFnI4CvOcnY68tfL5Vx9lDk/lZ33/F9vTu7m+42Jx6bWnzi9c+1rq1KEb50r/3PVF97JzDU3Huiq/jPVfK5//0NLIg84DJQ3Rt/aAxyuvPtNfXbjP+crWvqvfPNm7ZcH1j5vIzQ7zwEe9u8Q3gHJ55wvz573fMu3ul0pCwQ7psGfGuzPP7n/uefbrRVfCvx595Li8u++H1XNYX6S9cceJnf1d3b9/d/C5H4t+W/zBGefG0+reuRvf63zzsYEdB/d3nQhs3ff6pc8r6UPkT/P6Gs1t9wzkVe72nrnx9qfT/mh9+eyRJ9ZfiZXEL9w8tm7vATh3UcP8p0++CAKXerYNzLn/k2XPLtzp6L34cLTn6NJVDTNmX+463bbnl+ODZfwbn+Hc3RAUAAA="},
     }).then(res => res.json()).then(data => {
         saveEbay(data);
-        console.log(data.categoryId);
     });
 };
 
 
 
 var saveEbay = function(data){
-    console.log(data);
     var rawId = data.itemId;
     var newId = rawId.slice(3, 15);
     var itemExists = "";
